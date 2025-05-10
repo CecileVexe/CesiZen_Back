8deb16fbf274f6e8b607c9843fa8f479deec2043
CecileVexe <cecile.valte@gmail.com>
2025-05-10 12:31:56 +0200

    add several authorization

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:8081'],
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:8081',
+      'http://10.176.133.139:8081',
+      'http://192.168.1.153:8081',
+      'exp://192.168.1.153:8081',
+    ],
   });
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 void bootstrap();
